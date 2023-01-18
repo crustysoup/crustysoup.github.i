68cd5f4fec7e08a7f174daa16c7f5dae2e72701e
crustysoup <redheadhensley@gmail.com>
2023-01-17 18:05:53 -0600

    ae

---
+++ data/client-main/preload.js
@@ -0,0 +1,10 @@
+﻿const { ipcRenderer } = require('electron')
+
+function updateRPC(){
+	details = document.getElementById("CPA_GameSection").getAttribute("details");
+	state = document.getElementById("CPA_GameSection").getAttribute("state");
+
+	ipcRenderer.send('rpcupdate', details + "&" + state)
+}
+
+setInterval(updateRPC, 10000)
